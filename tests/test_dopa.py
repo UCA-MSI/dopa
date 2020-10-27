e38abb2fc8dfba997aa2617286782d1637c32718
marcomilanesio <milanesio.marco@gmail.com>
2020-10-27 14:28:11 +0100

    new tests for kwargs

--- tests/test_dopa.py
+++ tests/test_dopa.py
@@ -5,31 +5,49 @@ import dopa
 def func1(x):
     return x + 1
 
-def func2(x,y,z):
+
+def func2(x, y, z):
     return x + y + z
+
+
+def func3(x, y, z=0):
+    return func2(x, y, z)
 
 
 class TestDopa(unittest.TestCase):
     
     def setUp(self):
-        self.singlearg = [1,2,3]
-        self.multi = [(1,2,3), (4,5,6)]
+        self.singlearg = [1, 2, 3]
+        self.multi = [(1, 2, 3), (4, 5, 6)]
+        self.kw = [(1,2), (4,5,6)]
+        self.bad = [(1,2,3), 4]
         self.d = dict.fromkeys(self.singlearg, 2)
 
     def test_single_arg_thread(self):
-        res = dopa.do_parallel(self.singlearg, func1)
-        self.assertEqual(set(res), {2,3,4})
+        res = dopa.parallelize(self.singlearg, func1)
+        self.assertEqual(set(res), {2, 3, 4})
 
     def test_single_arg_process(self):
-        res = dopa.do_parallel(self.singlearg, func1, threaded=False)
-        self.assertEqual(set(res), {2,3,4})
+        res = dopa.parallelize(self.singlearg, func1, use_threads=False)
+        self.assertEqual(set(res), {2, 3, 4})
     
     def test_multiple_arg_thread(self):
-        res = dopa.do_parallel(self.multi, func2)
+        res = dopa.parallelize(self.multi, func2)
         self.assertEqual(set(res), {6, 15})
 
     def test_multiple_arg_process(self):
-        res = dopa.do_parallel(self.multi, func2, threaded=False)
+        res = dopa.parallelize(self.multi, func2, use_threads=False)
         self.assertEqual(set(res), {6, 15})
 
-        
+    def test_multiple_arg_kwarg(self):
+        res = dopa.parallelize(self.kw, func3)
+        self.assertEqual(set(res), {3, 15})
+
+    def test_check_all_same_type(self):
+        self.assertFalse(dopa._check_all_same_type(self.bad))
+        self.assertTrue(dopa._check_all_same_type(self.kw))
+
+    def test_check_argument_list(self):
+        with self.assertRaises(dopa.MalformedArgListError):
+            dopa._check_argument_list(self.kw, func2)
+        self.assertTrue(dopa._check_argument_list(self.kw, func3))
