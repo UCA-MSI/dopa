3d6871404e7d52abbdd875fa77fd32c3bbb0ac53
marcomilanesio <milanesio.marco@gmail.com>
2020-10-30 14:16:19 +0100

    added example with numpy and two arg function

--- examples/numpy.py
+++ examples/numpy.py
@@ -8,8 +8,15 @@ runs = [np.random.rand(3,2) for _ in range(10)]
 def func(mat):
     return mat.T
 
+def two_arg_func(mat1, mat2):
+    return np.dot(mat1, mat2)
+
 
 results = dopa.parallelize(runs, func)
 print( all([x.shape == (2,3) for x in results]))
 
+
+tup_runs = [(np.random.rand(3,2), np.random.rand(2,3) for _ in range(10)]
+results = dopa.parallelize(tup_runs, two_arg_func)
+print(results[0])
 
